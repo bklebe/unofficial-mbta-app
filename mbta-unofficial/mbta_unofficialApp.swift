e4a9989ada645b9fff6bd56b97879d86493db42c
Beatrix Klebe <beatrixk@hey.com>
2023-05-11 19:13:46 -0400

    Initial Commit

---
+++ mbta-unofficial/mbta_unofficialApp.swift
@@ -0,0 +1,17 @@
+//
+//  mbta_unofficialApp.swift
+//  mbta-unofficial
+//
+//  Created by Beatrix Klebe on 2023-05-11.
+//
+
+import SwiftUI
+
+@main
+struct mbta_unofficialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
